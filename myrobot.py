5c0dc75639391c65c4d19e1c11fe86ced8b3da72
Oliver <147245351+what-does-that-do@users.noreply.github.com>
2025-04-12 03:06:08 +0100

    Use drive_to, known bugs with new function for going half way!

--- myrobot.py
+++ myrobot.py
@@ -1,15 +1,16 @@
-from sr.robot3 import * # type: ignore
+from sr.robot3 import *  # type: ignore
 import time, math
 import util
 from datetime import datetime
 from pid import PID
 from camera import *
 
+
 class MyRobot:
-    __TARGET_MOTORS = [0,1]
+    __TARGET_MOTORS = [0, 1]
     __REVERSE = [1, 1]
     __COUNTS_PER_REVOL = 854.285714286
-    __REVOL_DIST = 0.392 # m
+    __REVOL_DIST = 0.392  # m
     __ACCURACY = 30
     __P, __I, __D = [0.01, 0, 0.00003]
     __ARDUINO_SERIAL = "7523031383335161A231"
@@ -29,11 +30,20 @@ class MyRobot:
     __LAC_MOVE_TIMES = [9, 8]
 
 
-    def __init__(self, revolDist = 0.392, targetMotors = [0,1], accuracy = 30, dbgEnabled = True, dbgPassThrough = False, noAccel = False):
+    def __init__(
+        self,
+        revolDist=0.392,
+        targetMotors=[0, 1],
+        accuracy=30,
+        dbgEnabled=True,
+        dbgPassThrough=False,
+        noAccel = False
+    ):
         self.__REVOL_DIST = revolDist
         self.__TARGET_MOTORS = targetMotors
         self.__ACCURACY = accuracy
         self.__SUB_ACCURACY = accuracy / 2
+
 
         self.__NO_ACCEL = noAccel
 
@@ -41,14 +51,14 @@ class MyRobot:
         self.__MOTOR_MB = self.ROBOT.motor_boards["SR0UK1L"]
         self.__PUMP_MB = self.ROBOT.motor_boards["SR0TJ1P"]
 
-        if self.ROBOT.mode == COMP: # type: ignore
+        if self.ROBOT.mode == COMP:  # type: ignore
             self.zone = self.ROBOT.zone
         else:
             self.zone = 2
 
         self.DEBUGGER = util.MyRobotDebug(enable=dbgEnabled, passThrough=dbgPassThrough)
         self.camera = MyRobotCamera(self.ROBOT, self.zone)
-        
+
         self.scissor_up()
 
     def __del__(self):
@@ -58,7 +68,7 @@ class MyRobot:
         self.__MOTOR_MB.motors[0].power = 0
         self.__MOTOR_MB.motors[1].power = 0
 
-    def __setReached(self, motors = [0,1]):
+    def __setReached(self, motors=[0, 1]):
         if 0 in motors:
             self.__MOTOR_MB.motors[0].power = 0
         if 1 in motors:
@@ -66,7 +76,6 @@ class MyRobot:
 
     def __clamp(self, pmin, x, pmax):
         return max(min(x, pmax), pmin)
-
     def __initialAccelCurve(self, endCount, currentCount):        
         return val if (val := (1- self.__INIT_PWR) / (endCount) * currentCount + self.__INIT_PWR > self.__INIT_PWR) else self.__INIT_PWR
     
@@ -81,11 +90,10 @@ class MyRobot:
 
         print(f"Factor: {fac}\n")
         """
-
         return pidObject(pCount)
 
     def __count_correct(self, m0Count, m1Count, target):
-        # MAYBE BAD      
+        # MAYBE BAD
         m0Count, m1Count = abs(m0Count), abs(m1Count)
 
         countDelta = abs(m0Count - m1Count)
@@ -108,18 +116,24 @@ class MyRobot:
         self.__M0FAC = 1
         self.__M1FAC = 1
 
-        print(f"Started drive: pDistance: {pDistance}, targets = {self.__TARGET_MOTORS}")
-        
+        print(
+            f"Started drive: pDistance: {pDistance}, targets = {self.__TARGET_MOTORS}"
+        )
+
         # Calculate target
         targetCount = int(self.__COUNTS_PER_REVOL * pDistance / self.__REVOL_DIST)
 
         print(f"Going to {targetCount}...\n")
 
         # Initialise PID objects
-        m0PID = PID(self.__P, self.__I, self.__D, setpoint = targetCount * self.__REVERSE[0])
-        m0PID.output_limits = (-1,1)
-        m1PID = PID(self.__P, self.__I, self.__D, setpoint = targetCount * self.__REVERSE[1])
-        m1PID.output_limits = (-1,1)
+        m0PID = PID(
+            self.__P, self.__I, self.__D, setpoint=targetCount * self.__REVERSE[0]
+        )
+        m0PID.output_limits = (-1, 1)
+        m1PID = PID(
+            self.__P, self.__I, self.__D, setpoint=targetCount * self.__REVERSE[1]
+        )
+        m1PID.output_limits = (-1, 1)
 
         m0reached = False
         m1reached = False
@@ -143,11 +157,11 @@ class MyRobot:
             # Get current motor count
             receivedData = self.ROBOT.arduino.command("m")
             message += f"[{time.time() - initialTime}]\n"
-            if receivedData[-1] != '|':
+            if receivedData[-1] != "|":
                 # Badly formatted counts cause problems
-                message += f"Bad message received \"{receivedData}\"\n"
+                message += f'Bad message received "{receivedData}"\n'
                 continue
-            
+
             message += f"Received Data: '{receivedData}'\n"
 
             m0Count, m1Count = list(map(int, receivedData[:-1].split(";")))
@@ -166,7 +180,7 @@ class MyRobot:
             # Get current motor speed
             m0χ, m1χ = m0δ / δt, m1δ / δt
 
-           # if not rotate:
+
             self.__count_correct(m0Count, m1Count, targetCount)
 
 
@@ -176,11 +190,10 @@ class MyRobot:
                 self.__M1FAC *= fac
                 print(f"Factor: {fac}\n")
 
-
-
             if 0 in self.__TARGET_MOTORS and not m0reached:
                 self.__MOTOR_MB.motors[0].power = (m0Power := dampenFactor * self.__M0FAC * self.__powerByCount(m0Count, m0PID, targetCount, rotate))
                 message += f"M0: Count: {m0Count}, M0Fac: {self.__M0FAC}, M0Power; {m0Power}\n"
+
 
                 if abs(m0χ) < 20:
                     sCount0 += 1
@@ -189,6 +202,7 @@ class MyRobot:
                 if m0reached:
                     message += "Stopped M0\n"
                     self.__setReached(motors=[0])
+
 
             if  1 in self.__TARGET_MOTORS and not m1reached:
                 self.__MOTOR_MB.motors[1].power = (m1Power := dampenFactor * self.__M1FAC * self.__powerByCount(m1Count, m1PID, targetCount, rotate))
@@ -205,11 +219,12 @@ class MyRobot:
             m0LastCount = m0Count
             m1LastCount = m1Count
             print(message)
-            #self.DEBUGGER.debug(message)
+            # self.DEBUGGER.debug(message)
             message = ""
             time.sleep(0.005)
 
         self.stop()
+     #   self.ROBOT.sleep(.5)
         self.ROBOT.sleep(.5)
 
     def __RobotRotate(self, pAngle, SPECIAL_κ):
@@ -219,36 +234,37 @@ class MyRobot:
             fac = 0.5
 
         self.DEBUGGER.debug(f"Started rotate by {pAngle}...")
-        arcRadius = 0.425 # m
-        halfArc = arcRadius * math.pi # m
+        arcRadius = 0.425  # m
+        halfArc = arcRadius * math.pi  # m
 
-        #SPECIAL_κ = 1.165890625 #1.0127125 # Old: 1.0087125 ## DO NOT CHANGE!
+        # SPECIAL_κ = 1.165890625 #1.0127125 # Old: 1.0087125 ## DO NOT CHANGE!
+
 
         self.__RobotDrive(halfArc * (pAngle / 180) * 0.5 * SPECIAL_κ, rotate=True, dampenFactor = fac)
-        
+
     def __setLacState(self, v):
         self.__PUMP_MB.motors[1].power = v
-    
+
     def __getLacState(self):
         return self.__PUMP_MB.motors[1].power
-    
+
     def __getLacCurrentDraw(self):
         return self.__PUMP_MB.motors[1].current
-    
+
     def __getPumpCurrentDraw(self):
         return self.__PUMP_MB.motors[0].current
-    
+
     def getPUMP_MB(self):
         return self.__PUMP_MB
-    
+
     @property
     def pump(self):
         return self.__PUMP_MB.motors[0].power != 0
-    
+
     @pump.setter
     def pump(self, enabled):
         self.__PUMP_MB.motors[0].power = -1 if enabled else 0
-    
+
     def scissor_up(self, dur=-1):
         print("Scissor going up...")
         self.__setLacState(1)
@@ -259,7 +275,7 @@ class MyRobot:
                 self.sleep(0.1)
         self.__setLacState(0)
         print("Scissor is up.")
-        
+
     def scissor_down(self, dur=-1):
         print("Scissor going down...")
         self.__setLacState(-1)
@@ -275,9 +291,9 @@ class MyRobot:
         outliers = 0
         for _ in range(5):
             if self.__getPumpCurrentDraw() > 0.7:
-                outliers+=1
+                outliers += 1
             self.sleep(0.1)
-        
+
         # print(outliers)
         return outliers > 0
 
@@ -299,51 +315,51 @@ class MyRobot:
         else:
             self.__RobotDrive(-distance)
 
-    def right(self, angle, isRadians = False):
+    def right(self, angle, isRadians=False):
         self.__REVERSE = [-1, 1]
         if isRadians:
             self.__RobotRotate(angle * (180 / math.pi), 1.1325)
         else:
             self.__RobotRotate(angle, 1.1325)
 
-    def left(self, angle, isRadians = False):
+    def left(self, angle, isRadians=False):
         self.__REVERSE = [1, -1]
         if isRadians:
             self.__RobotRotate(angle * (180 / math.pi), 1.29725)
         else:
             self.__RobotRotate(angle, 1.29725)
-    
+
     def grab(self):
         self.pump = True
         self.scissor_down()
         self.sleep(1)
         self.scissor_up()
-    
+
     def drop(self):
         self.pump = False
-    
+
     def place(self):
         self.scissor_down()
         self.pump = False
         self.scissor_up()
-    
+
     def beep(self, hz=440, dur=0.1):
         self.ROBOT.power_board.piezo.buzz(hz, dur)
-    
+
     def beep_sync(self, hz=440, dur=0.1, timeout=0):
         self.ROBOT.power_board.piezo.buzz(hz, dur, blocking=True)
         if timeout > 0:
             self.ROBOT.sleep(timeout)
-    
+
     def sleep(self, length):
         self.ROBOT.sleep(length)
 
     def see_and_capture(self, f):
         return self.ROBOT.camera.see(save=f)
-    
-    def marker_list_contains_id(self, marker_list:list, marker) -> bool:
-        return self.marker_list_get_marker_with_id(marker_list, marker.id) != None
-    
+
+    def marker_list_contains_id(self, marker_list: list, marker) -> bool:
+        return (self.marker_list_get_marker_with_id(marker_list, marker.id) != None)
+
     def marker_list_get_marker_with_id(self, marker_list: list, marker_id):
         for m in marker_list:
             if m.id == marker_id:
@@ -352,25 +368,26 @@ class MyRobot:
 
     def drive_to(self, target_marker) -> bool:
         if target_marker.position.distance > 1000:
-            self.forward(target_marker.position.distance * 0.75)
+            self.forward(((target_marker.position.distance / 1000) - 0.2) * 0.75)
             visible_markers = self.camera.find_all_markers()
+
             if self.marker_list_contains_id(visible_markers, target_marker):
                 self.right(15)
                 visible_markers = self.camera.find_all_markers()
+
                 if self.marker_list_contains_id(visible_markers, target_marker):
                     self.left(30)
                     visible_markers = self.camera.find_all_markers()
+
                     if self.marker_list_contains_id(visible_markers, target_marker):
                         return False
-            new_target_marker = self.marker_list_get_marker_with_id(visible_markers, target_marker)
+            
+            new_target_marker = self.marker_list_get_marker_with_id(
+                visible_markers, target_marker.id
+            )
+
             self.right(new_target_marker.position.horizontal_angle, True)
-            self.forward(new_target_marker.position.distance / 1000 - 0.2)
+            self.forward((new_target_marker.position.distance / 1000) - 0.2)
         else:
             self.right(target_marker.position.horizontal_angle, True)
-            self.forward(new_target_marker.position.distance / 1000 - 0.2)
-
-
-
-            
-
-            
+            self.forward((target_marker.position.distance / 1000) - 0.2)
--- robot.py
+++ robot.py
@@ -8,34 +8,35 @@ import enum
 
 TESTING = False
 tests_to_run = [
-    #"drive_back_forwards",
-    #"lots_of_rotation_left",
-    #"lots_of_rotation_right",
-    #"rotate_20",
+    # "drive_back_forwards",
+    # "lots_of_rotation_left",
+    # "lots_of_rotation_right",
+    # "rotate_20",
     # "drive_full",
-    #"drive_reverse",
-    #"drive_back_forwards",
-    #"drive_back_forwards",
-    #"navigate_to_cube",
-   # "drive_right_triangle",
+    # "drive_reverse",
+    # "drive_back_forwards",
+    # "drive_back_forwards",
+    # "navigate_to_cube",
+    # "drive_right_triangle",
     # "lac_up_down",
     # "lac_time_up_down",
     # "pump_on_off",
     # "stand_and_deliver",
-    #"full_scissor_test",
+    # "full_scissor_test",
     # "pump_read_current",
     # "scissor_read_current",
     # "rickroll",
     # "motor_challenge",
     # "play_fetch",
     # "pump_toggler"
-    #"back_forwards_12_billion",
+    # "back_forwards_12_billion",
     # "up_down_12_billion",
-    #"game_tests"
+    # "game_tests"
 ]
 
 if TESTING:
-    import tests #Avoid double robot declaration by importing here
+    import tests  # Avoid double robot declaration by importing here
+
     unittest.main(module=tests.TestRobot, verbosity=2, defaultTest=tests_to_run)
     # unittest.main exists after completion
 
@@ -47,24 +48,26 @@ print("Hello! I'm looking for high rise:", TARGET_HIGHRISE)
 runningCompetition = True
 visitedMarkers = []
 
+
 class RobotState(enum.Enum):
     LOOKING_FOR_CUBES = enum.auto()
     GRABBING = enum.auto()
     LOOKING_FOR_DISTRICT = enum.auto()
     PLACE = enum.auto()
 
+
 state = RobotState.LOOKING_FOR_CUBES
 total_search_rotation = 0
 while runningCompetition:
     print("Current state:", state)
 
-    if (state == RobotState.LOOKING_FOR_CUBES):
+    if state == RobotState.LOOKING_FOR_CUBES:
         # find a pallet marker to navigate to
         # |  || || |_
         markers = robot.camera.find_all_markers()
         print("Markers:", markers)
 
-        '''
+        """
         ||||
         ||||
         ||||
@@ -79,10 +82,12 @@ while runningCompetition:
             robot.reverse(1)
             total_search_rotation = 0
             continue
-        '''
+        """
 
-        pallet_markers = robot.camera.check_if_markers_are_our_pallets(markers, ignored_markers=visitedMarkers)
-        
+        pallet_markers = robot.camera.check_if_markers_are_our_pallets(
+            markers, ignored_markers=visitedMarkers
+        )
+
         # if we have turned all the way around, not found anything, reverse.
         if total_search_rotation > 360:
             print("No pallets we are interested in could be found, reversing.")
@@ -92,7 +97,6 @@ while runningCompetition:
         if len(pallet_markers) == 0:
             robot.right(20)
             total_search_rotation += 20
-        
         else:
             print("Pallet found.")
             total_search_rotation = 0
@@ -100,40 +104,19 @@ while runningCompetition:
             pallet = pallet_markers[0]
             print("Going to", pallet)
 
-            robot.right(pallet.position.horizontal_angle, isRadians=True)
-            
-            # stop and re-calibrate at half way if we are further than a meter away from target.
-            if pallet.position.distance > 1000:
-                robot.forward((((pallet.position.distance) / 1000) - 0.15) * 0.5)
+            robot.drive_to(pallet)
 
-                print("stopping and correcting...")
-                markers = robot.camera.find_all_markers()
-                pallet_changed = False
-
-                for m in markers:
-                    if m.id == pallet.id:
-                        pallet = m
-                        pallet_changed = True
-                        print("Updated pallet:", pallet)
-                        break
-                
-                if not pallet_changed:
-                    print("Aborting, can't find pallet anymore!")
-                    continue
-            else:
-                robot.forward(((pallet.position.distance) / 1000) - 0.15)
-            
             robot.right(pallet.position.horizontal_angle, isRadians=True)
             robot.forward(((pallet.position.distance) / 1000) - 0.15)
 
             print("I think I've arrived at", pallet)
             state = RobotState.GRABBING
 
-    elif (state == RobotState.GRABBING):
+    elif state == RobotState.GRABBING:
         print("Grabbing pallet...")
         robot.grab()
 
-        '''
+        """
         ||||
         ||||
         ||||
@@ -143,9 +126,9 @@ while runningCompetition:
 
         Please uncomment the pump grabbing code for the competition!
         
-        '''
+        """
 
-        '''if (robot.pump_grabbing_noise_based()):
+        """if (robot.pump_grabbing_noise_based()):
             visitedMarkers.append(pallet)
             print("Grabbed pallet :)")
             state = RobotState.LOOKING_FOR_DISTRICT
@@ -153,13 +136,15 @@ while runningCompetition:
             print("Cube grab failed. Restarting...")
             robot.drop()
             robot.reverse(0.5)
-            state = RobotState.LOOKING_FOR_CUBES'''
+            state = RobotState.LOOKING_FOR_CUBES"""
 
         visitedMarkers.append(pallet)
-        print("Added pallet to visited markers. Not checking for pallet actually grabbed!")
+        print(
+            "Added pallet to visited markers. Not checking for pallet actually grabbed!"
+        )
         state = RobotState.LOOKING_FOR_DISTRICT
-    
-    elif (state == RobotState.LOOKING_FOR_DISTRICT):
+
+    elif state == RobotState.LOOKING_FOR_DISTRICT:
         # camera find all markers
         markers = robot.camera.find_all_markers()
 
@@ -169,7 +154,7 @@ while runningCompetition:
             if marker.id == TARGET_HIGHRISE:
                 plinth = marker
                 break
-        
+
         print("Plinth found:", plinth)
 
         if plinth and robot.camera.is_pallet_on_plinth(markers, TARGET_HIGHRISE):
@@ -187,7 +172,7 @@ while runningCompetition:
                 if marker.id in visitedMarkers:
                     target = marker
                     break
-            
+
             # turn if nothing found
             if not target:
                 robot.right(15)
@@ -196,8 +181,8 @@ while runningCompetition:
                 robot.right(target.position.horizontal_angle, isRadians=True)
                 robot.forward(((target.position.distance) / 1000))
                 state = RobotState.PLACE
-    
-    elif (state == RobotState.PLACE):
+
+    elif state == RobotState.PLACE:
         robot.place()
         robot.reverse(1)
         state = RobotState.LOOKING_FOR_CUBES
